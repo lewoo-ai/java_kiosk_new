0e85b31f5870eea04e9a9e2614461eb89c5f376c
leewoosuk <dldntjr100@naver.com>
2023-11-23 07:55:48 +0900

    컨트롤러

--- src/main/java/kr/ac/mjc/kiosk/controller/FrontController.java
+++ src/main/java/kr/ac/mjc/kiosk/controller/FrontController.java
@@ -1,11 +1,40 @@
 package kr.ac.mjc.kiosk.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import kr.ac.mjc.kiosk.domain.Product;
+import kr.ac.mjc.kiosk.service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-@RequestMapping("/kiosk")
 
 public class FrontController {
+
+    private final CustomerService cs;
+
+    @Autowired
+    public FrontController(CustomerService cs) {
+        this.cs = cs;
+    }
+
+    @GetMapping("/")
+    public String home() {
+        return "index"; // 이 부분은 웹 페이지의 이름 또는 경로로 수정해야 합니다.
+    }
+
+
+    @GetMapping("/products")
+    public ResponseEntity<List<Product>> findProduct(){
+        List<Product> products =CustomerService.findAll();
+        return ResponseEntity.ok().body(products);
+    }
+
+
+
+
+
 
 }

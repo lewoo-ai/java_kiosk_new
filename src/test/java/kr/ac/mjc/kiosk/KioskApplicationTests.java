bd97f1eb0ee33d80fc9ae754da85560351e45a1d
leewoosuk <dldntjr100@naver.com>
2023-11-23 06:20:09 +0900

    first commit

---
+++ src/test/java/kr/ac/mjc/kiosk/KioskApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.ac.mjc.kiosk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KioskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

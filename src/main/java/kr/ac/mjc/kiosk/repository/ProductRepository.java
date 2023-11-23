413c0c87ed29a6c80a87138c4339fdac2cf4e103
[leewoo-ai] <[dldntjr100@naver.com]>
2023-11-23 13:16:55 +0900

    상품 리포지토리

--- src/main/java/kr/ac/mjc/kiosk/repository/ProductRepository.java
+++ src/main/java/kr/ac/mjc/kiosk/repository/ProductRepository.java
@@ -1,4 +1,10 @@
 package kr.ac.mjc.kiosk.repository;
 
-public interface ProductRepository {
+import kr.ac.mjc.kiosk.domain.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product,Integer> {
+
+
+
 }

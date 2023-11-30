eef55483e36083075cd3ceb5ccfc36a604a3901b
[leewoo-ai] <[dldntjr100@naver.com]>
2023-11-30 13:02:29 +0900

    변경함

--- src/main/java/kr/ac/mjc/kiosk/dto/ItemForm.java
+++ src/main/java/kr/ac/mjc/kiosk/dto/ItemForm.java
@@ -14,4 +14,19 @@ public class ItemForm {
     @NotEmpty
     private String productId;
 
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getProductId() {
+        return productId;
+    }
+
+    public void setProductId(String productId) {
+        this.productId = productId;
+    }
 }

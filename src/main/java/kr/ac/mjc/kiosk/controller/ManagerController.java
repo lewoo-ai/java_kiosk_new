61e931cb8776a71d057f9e5589c5d0263571e4ab
kishmin <kishmin@naver.com>
2023-12-07 14:24:05 +0900

    매니저 컨트롤러 수정, 유틸 추가, 리소스에 properties 추가

--- src/main/java/kr/ac/mjc/kiosk/controller/ManagerController.java
+++ src/main/java/kr/ac/mjc/kiosk/controller/ManagerController.java
@@ -1,42 +1,90 @@
 package kr.ac.mjc.kiosk.controller;
 
-import kr.ac.mjc.kiosk.domain.Category; // Category 클래스로 변경
-import kr.ac.mjc.kiosk.domain.Orders; // Orders 클래스로 변경
-import kr.ac.mjc.kiosk.domain.OrdersDetails; // OrdersDetails 클래스로 변경
-import kr.ac.mjc.kiosk.service.CustomerService;
+import kr.ac.mjc.kiosk.domain.Bundle;
+import kr.ac.mjc.kiosk.domain.Orders;
+import kr.ac.mjc.kiosk.domain.Product;
+import kr.ac.mjc.kiosk.service.ManagerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin
 @RestController
-@RequestMapping("/customer")
-public class CustomerController {
-
-    private final CustomerService customerService;
+@CrossOrigin
+@RequestMapping("/manager")
+public class ManagerController {
 
     @Autowired
-    public CustomerController(CustomerService customerService) {
-        this.customerService = customerService;
-    }
+    private ManagerService managerService;
 
-    @GetMapping("/products")
-    public List<Category> getProductsByCategory() {
-        return customerService.selectProductByCategory();
-    }
-
-    @PostMapping("/orders")
-    public ResponseEntity<?> insertOrders(@RequestBody List<OrdersDetails> ordersDetailsList) {
-        Orders orders = new Orders();
-        orders.setOrdersDetailsList(ordersDetailsList);
-
+    @PostMapping("/product")
+    public ResponseEntity<String> addProduct(@RequestBody Product product) {
         try {
-            customerService.insertOrders(orders);
-            return ResponseEntity.ok("Orders inserted successfully.");
+            managerService.productInsert(product);
+            return ResponseEntity.ok("Product added successfully.");
         } catch (Exception e) {
-            return ResponseEntity.badRequest().body("Failed to insert orders.");
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @PutMapping("/product")
+    public ResponseEntity<String> updateProduct(@RequestBody Product product) {
+        try {
+            managerService.productUpdate(product);
+            return ResponseEntity.ok("Product updated successfully.");
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @DeleteMapping("/product/{productCode}")
+    public ResponseEntity<String> deleteProduct(@PathVariable String productCode) {
+        try {
+            managerService.productDelete(productCode);
+            return ResponseEntity.ok("Product deleted successfully.");
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @PostMapping("/bundle")
+    public ResponseEntity<String> addBundle(@RequestBody Bundle bundle) {
+        try {
+            managerService.bundleInsert(bundle);
+            return ResponseEntity.ok("Bundle added successfully.");
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @PutMapping("/bundle")
+    public ResponseEntity<String> updateBundle(@RequestBody Bundle bundle) {
+        try {
+            managerService.bundleUpdate(bundle);
+            return ResponseEntity.ok("Bundle updated successfully.");
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @DeleteMapping("/bundle/{bundleCode}")
+    public ResponseEntity<String> deleteBundle(@PathVariable String bundleCode) {
+        try {
+            managerService.bundleDelete(bundleCode);
+            return ResponseEntity.ok("Bundle deleted successfully.");
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @GetMapping("/orders")
+    public ResponseEntity<List<Orders>> getOrders() {
+        try {
+            List<Orders> ordersList = managerService.ordersSelect();
+            return ResponseEntity.ok(ordersList);
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(null);
         }
     }
 }

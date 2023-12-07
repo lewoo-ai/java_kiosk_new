44e50230a5423848377bce296d6e405fd0390bef
realkkangs <realkkangs@gmail.com>
2023-12-07 11:58:45 +0900

    sdaad

--- Front/src/app/parts/navigation/navigation.ts
+++ Front/src/app/parts/navigation/navigation.ts
@@ -7,8 +7,8 @@ import {Role} from '../../enum/Role';
 
 @Component({
     selector: 'app-navigation',
-    templateUrl: './navigation.component.html',
-    styleUrls: ['./navigation.component.css']
+    templateUrl: './navigation.html',
+    styleUrls: ['./navigation.css']
 })
 export class NavigationComponent implements OnInit, OnDestroy {
 
--- Front/src/app/parts/pagiination/pagination.ts
+++ Front/src/app/parts/pagiination/pagination.ts
@@ -2,8 +2,8 @@ import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
     selector: 'app-pagination',
-    templateUrl: './pagination.component.html',
-    styleUrls: ['./pagination.component.css']
+    templateUrl: './pagination.html',
+    styleUrls: ['./pagination.css']
 })
 export class PaginationComponent implements OnInit {
 

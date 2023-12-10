8d78d66b6abe78b3820e745c4169c77a13347a78
Woosuk <dldntjr100@naver.com>
2023-12-10 18:19:41 +0900

    수정

--- src/main/java/kr/ac/mjc/kiosk/controller/KioskViewController.java
+++ src/main/java/kr/ac/mjc/kiosk/controller/KioskViewController.java
@@ -1,82 +1,61 @@
 package kr.ac.mjc.kiosk.controller;
 
+import kr.ac.mjc.kiosk.domain.OrderDetails;
 import kr.ac.mjc.kiosk.dto.CategoryDto;
 import kr.ac.mjc.kiosk.dto.ProductDto;
 import kr.ac.mjc.kiosk.service.CategoryService;
+import kr.ac.mjc.kiosk.service.OrderDetailsService;
+import kr.ac.mjc.kiosk.service.OrderService;
 import kr.ac.mjc.kiosk.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Optional;
 
 @Controller
+@RequestMapping("/kiosk")
 public class KioskViewController {
 
     private final ProductService productService;
     private final CategoryService categoryService;
+    private final OrderService orderService;
+    private final OrderDetailsService orderDetailsService;
 
     @Autowired
-    public KioskViewController(ProductService productService, CategoryService categoryService) {
+    public KioskViewController(ProductService productService, CategoryService categoryService, OrderService orderService, OrderDetailsService orderDetailsService) {
         this.productService = productService;
         this.categoryService = categoryService;
+        this.orderService = orderService;
+        this.orderDetailsService = orderDetailsService;
     }
 
-    @GetMapping("/kiosk")
-    public String showKioskMainPage(Model model) {
-        List<CategoryDto> categories = categoryService.getAllCategories();
-        List<ProductDto> products = productService.getAllProducts();
-
-        model.addAttribute("categories", categories);
-        model.addAttribute("products", products);
-
-        return "KioskView"; // Assuming you have a Thymeleaf template named kioskMain.html
-    }
-
-    @GetMapping("/kiosk/products")
-    public String showProducts(Model model) {
+    @GetMapping("/products")
+    public String showAllProducts(Model model) {
         List<ProductDto> products = productService.getAllProducts();
         model.addAttribute("products", products);
-        return "productList"; // Assuming you have a Thymeleaf template named productList.html
+        return "products";
     }
 
-    @GetMapping("/kiosk/products/{id}")
-    public String showProductDetails(@PathVariable Long id, Model model) {
-        Optional<ProductDto> productOptional = productService.getProductById(id);
-
-        if (productOptional.isPresent()) {
-            ProductDto product = productOptional.get();
-            model.addAttribute("product", product);
-            return "productDetails";
-        } else {
-            // 처리할 내용 (예: 해당 ID에 해당하는 제품이 없을 때의 에러 페이지로 이동)
-            return "errorPage";
-        }
-    }
-
-    @GetMapping("/kiosk/categories")
-    public String showCategories(Model model) {
+    @GetMapping("/categories")
+    public String showAllCategories(Model model) {
         List<CategoryDto> categories = categoryService.getAllCategories();
         model.addAttribute("categories", categories);
-        return "categoryList"; // Assuming you have a Thymeleaf template named categoryList.html
+        return "categories";
     }
 
-    @GetMapping("/kiosk/categories/{id}")
-    public String showCategoryDetails(@PathVariable Long id, Model model) {
-        Optional<CategoryDto> categoryOptional = categoryService.getCategoryById(id);
-
-        if (categoryOptional.isPresent()) {
-            CategoryDto category = categoryOptional.get();
-            model.addAttribute("category", category);
-            return "categoryDetails";
-        } else {
-            // 처리할 내용 (예: 해당 ID에 해당하는 카테고리가 없을 때의 에러 페이지로 이동)
-            return "errorPage";
-        }
+    @GetMapping("/categories/{categoryCode}/products")
+    public String showProductsByCategoryCode(@PathVariable String categoryCode, Model model) {
+        List<ProductDto> products = categoryService.getProductsByCategoryCode(categoryCode);
+        model.addAttribute("products", products);
+        return "category-products";
     }
 
-    // You can add more methods for other views or functionalities as needed.
+    @GetMapping("/orders/{orderId}")
+    public String showOrderDetailsByOrderId(@PathVariable Long orderId, Model model) {
+        List<OrderDetails> orderDetails = orderDetailsService.getOrderDetailsByOrderId(orderId);
+        model.addAttribute("orderDetails", orderDetails);
+        return "order-details";
+    }
 }

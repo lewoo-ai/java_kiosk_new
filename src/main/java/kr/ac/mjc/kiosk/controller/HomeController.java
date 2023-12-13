0f3a047273eab427473dba1a7760e267b8d6c33a
Woosuk <dldntjr100@naver.com>
2023-12-13 21:58:10 +0900

    수정

--- src/main/java/kr/ac/mjc/kiosk/controller/HomeController.java
+++ src/main/java/kr/ac/mjc/kiosk/controller/HomeController.java
@@ -1,2 +1,14 @@
-package kr.ac.mjc.kiosk.controller;public class HomeController {
+package kr.ac.mjc.kiosk.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String home() {
+        return "index";
+    }
 }

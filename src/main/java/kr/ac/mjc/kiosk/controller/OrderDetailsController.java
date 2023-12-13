62c9cc203ae38e11ddf2bc85c789e2e0ed76b4a9
ChaeMinYun <kishmin@naver.com>
2023-12-13 21:55:17 +0900

    수정

--- src/main/java/kr/ac/mjc/kiosk/controller/OrderDetailsController.java
+++ src/main/java/kr/ac/mjc/kiosk/controller/OrderDetailsController.java
@@ -2,7 +2,6 @@ package kr.ac.mjc.kiosk.controller;
 
 import kr.ac.mjc.kiosk.domain.OrderDetails;
 import kr.ac.mjc.kiosk.service.OrderDetailsService;
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.ui.Model;
@@ -11,7 +10,6 @@ import java.util.List;
 
 @Controller
 @RequestMapping("/api/order-details")
-@RequiredArgsConstructor
 public class OrderDetailsController {
 
     private final OrderDetailsService orderDetailsService;
@@ -24,11 +22,10 @@ public class OrderDetailsController {
     public List<OrderDetails> getOrderDetailsByOrderId(@PathVariable Long orderId) {
         return orderDetailsService.getOrderDetailsByOrderId(orderId);
     }
-        @GetMapping("/{orderId}")
-        public String showOrderDetailsPage(@PathVariable Long orderId, Model model) {
-            List<OrderDetails> orderDetails = orderDetailsService.getOrderDetailsByOrderId(orderId);
-            model.addAttribute("orderDetails", orderDetails);
-            return "order-details";
-        }
+    public String showOrderDetailsPage(@PathVariable Long orderId, Model model) {
+        List<OrderDetails> orderDetails = orderDetailsService.getOrderDetailsByOrderId(orderId);
+        model.addAttribute("orderDetails", orderDetails);
+        return "order-details";
     }
+}
 
